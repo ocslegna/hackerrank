7422056ee0d969513e32f58af7ed7d5d047174ee
Gabriel Scotillo <gabrielscotillo@gmail.com>
2017-07-07 05:30:42 -0300

    Testing vim.

--- hackerrank/Python/Built_ins/ginortS.py
+++ hackerrank/Python/Built_ins/ginortS.py
@@ -32,3 +32,4 @@ Hint: Success is not the key, but  is success.
 
 if __name__ == '__main__':
     print(*sorted(input(), key=lambda x: (x.isdigit(), x.isdigit() and int(x) % 2 == 0, x.isupper(), x.islower(), x)), sep = '')
+
--- hackerrank/Python/Built_ins/sort_data.py
+++ hackerrank/Python/Built_ins/sort_data.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-"""
+"""Summary line.
+
 You are given data in a tabular format.
 The data contains N rows, and each row contains M space separated elements.
 You can imagine the M items to be different attributes,
- (like height, weight, energy, etc.) and each of the N rows as an instance or a sample.
+ (like height, weight, energy, etc.) and
+ each of the N rows as an instance or a sample.
 
-Your task is to sort the table on the Kth attribute and print the final resulting table.
+Your task is to sort the table on the Kth
+ attribute and print the final resulting table.
 Note: If two attributes are the same for different rows,
  print the row that appeared first in the input.
 __
