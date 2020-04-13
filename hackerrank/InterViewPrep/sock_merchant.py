d8b4192522ee6f0e07de558320d44f394fb802fa
ocslegna <gabrielscotillo@gmail.com>
2020-04-13 17:27:08 -0300

    test PyCharm

--- hackerrank/InterViewPrep/sock_merchant.py
+++ hackerrank/InterViewPrep/sock_merchant.py
@@ -8,16 +8,13 @@ import sys
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
+    pass
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sockMerchant(n, ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
